b99d3beccfcf627ac137da94ff9a5c6e1f7444b0
Patrick Yambrick <patrickyambrick@gmail.com>
2023-11-29 20:06:45 -0500

    Style nav component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,44 @@ import React, { ReactNode } from 'react';
 import Nav from './Nav';
 import ToggleButton from './ToggleButton';
 import styles from '../styles/Layout.module.scss';
+import { useEffect, useState } from 'react';
+
+import { getTheme, setTheme } from '../redux/themeSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Layout = ({ children }: { children: ReactNode }) => {
+	const dispatch = useDispatch();
+	const theme = useSelector(getTheme);
+	const [themeLoaded, setThemeLoaded] = useState(false);
+
+	useEffect(() => {
+		if (!themeLoaded) {
+			// Try to get the theme from localStorage
+			const storedTheme = localStorage.getItem('theme');
+
+			if (storedTheme) {
+				// If the theme is found in localStorage, set it in the Redux store
+				dispatch(setTheme(storedTheme));
+			}
+
+			// Mark the theme as loaded
+			setThemeLoaded(true);
+		}
+	}, [dispatch, themeLoaded]);
+	if (!themeLoaded) {
+		// If theme is not loaded yet, you can render a loading state or return null
+		return null;
+	}
 	return (
 		<>
-			<div className={styles.container}>
-				<Nav links={['Home', 'Monstars', 'Tune Squad']} />
+			<div
+				className={`${styles.container} ${
+					theme === 'modern' ? styles.modern : styles.retro
+				}`}
+			>
 				<ToggleButton />
+
+				<Nav links={['Home', 'Monstars', 'Tune Squad']} />
 			</div>
 
 			{children}
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,40 @@
 import Link from 'next/link';
-import styles from '../styles/Card.module.scss';
+import styles from '../styles/Nav.module.scss';
+import { NavProps } from '../utils/types';
+import { useEffect, useState } from 'react';
 
-export default function Nav(props: any) {
+import { getTheme, setTheme } from '../redux/themeSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
+export default function Nav(props: NavProps) {
+	const dispatch = useDispatch();
+	const theme = useSelector(getTheme);
+	const [themeLoaded, setThemeLoaded] = useState(false);
+
+	useEffect(() => {
+		if (!themeLoaded) {
+			// Try to get the theme from localStorage
+			const storedTheme = localStorage.getItem('theme');
+
+			if (storedTheme) {
+				// If the theme is found in localStorage, set it in the Redux store
+				dispatch(setTheme(storedTheme));
+			}
+
+			// Mark the theme as loaded
+			setThemeLoaded(true);
+		}
+	}, [dispatch, themeLoaded]);
+	if (!themeLoaded) {
+		// If theme is not loaded yet, you can render a loading state or return null
+		return null;
+	}
 	return (
-		<nav>
+		<nav
+			className={`${styles.nav} ${
+				theme === 'modern' ? styles.modern : styles.retro
+			}`}
+		>
 			<ul>
 				{props.links.map((link: string, index: number) => {
 					if (link !== 'Home') {
